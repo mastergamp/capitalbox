84efb794cd1199f400a47824afb5002aab6ff6c3
Ivan <godofluck89@gmail.com>
2016-02-11 22:54:26 +0300

    small fix

--- config.js
+++ config.js
@@ -3,11 +3,6 @@ module.exports = {
 		path: __dirname + "/modules/db"
 	},
 	"google": {
-		"CLIENT_ID" : "967086329663-edojfe3drrcc083toh72rtsbag7a7lm0.apps.googleusercontent.com",
-		"CLIENT_SECRET" : "U0Ua6HMl_c1kMsddH_oWsX65",
-		"REDIRECT_URL" : ["urn:ietf:wg:oauth:2.0:oob","http://localhost"],
-		"ACCESS_TYPE" : "offline",
-		"SCOPE" : "https://www.googleapis.com/auth/drive",
 		"DBFolder": "0B0-8vnNvFNZTV0o5NW9qelJxeVk"
 	}
 };
